2c27d0c7fc60b2d9a4045e630fb31c5d7634267b
Sebastián Medina <115100456+sebasmedina2003@users.noreply.github.com>
2022-12-06 16:35:55 -0400

    Update main.py

--- Logica simbolica 2/main.py
+++ Logica simbolica 2/main.py
@@ -2,11 +2,4 @@ from modulos.manejador_db import *
 from modulos.constantes import *
 from tkinter import *
 
-class ventana(Tk):
-	def __init__(self):
-		lbl = Label(self, text = "Hola")
-		lbl.grid(row = 0, column = 0)
-
-if __name__ == "__main__":
-	ventana = ventana()
-	ventana.title("Hola mundo")
+print("Hola mundo")
